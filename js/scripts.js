e43c1134e966adbab58c5d6583bc1b1f1fabc679
gabriel nwachukwu <gabrielcoder247@gmail.com>
2018-05-09 15:58:46 +0300

    add checkbxes

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,22 @@
-
 //business logic
-function Movie (movie,time, age,gender){
+function Movie(movie, time, age, gender) {
   this.movie = movie;
   this.time = time;
   this.age = age;
-  this.gender = gender;
+  this.gender=gender;
 
 }
 // user interface
-$(documents.ready(function(){
-$("form#movie-time").submit(function(event){
-  event.preventDefault();
-  var newMovieTitle = $("input#movie-title").val();
-  var newMovieTime = $("input#movie-time").val();
-  var newMovieAge = $("input#age-of-viewer").val();
-  var newMovieGenderMale = $("input#male-check-box").val();
-  var newMovieGenderFemale = $("input#female-check-box").val();
-  var newMovie = new Movie(newMovieTitle, newMovieTime, newMovieAge, newMovieGenderMale, newMovieGenderFemale);
-  $("ul#movie-ticket-price").append("<li><span>" + newMovie.movie + "</li></span>");
-});
-
-
+$(document).ready(function() {
+      $("#movie-ticket").submit(function(event) {
+        event.preventDefault();
+        // console.log('done');
+        var newMovieTitle = $("input#movie-title").val();
+        var newMovieTime = $("input#movie-time").val();
+        var newMovieAge = $("input#age-of-viewer").val();
+        var newMovieGenderMale = $("input#male-check-box").val();
+        var newMovieGenderFemale = $("input#female-check-box").val();
+        var newMovie = new Movie(newMovieTitle, newMovieTime, newMovieAge, newMovieGenderMale);
+        $("ul#movie-ticket-price").append("<li><span>" + newMovie.movie + "</span></li>");
+      });
 });
